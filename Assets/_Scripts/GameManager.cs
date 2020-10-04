75ca624c42863b3ea93b65c5acc35f49291f0030
Matias Abarquez Mendoza <matias.abarquez@hotmail.com>
2020-10-04 05:58:20 -0300

    Added GameManager

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public Canvas canvas;
+    public int HP = 3;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            //If I am the first instance, make me the Singleton
+            instance = this;
+            DontDestroyOnLoad(this);
+            DontDestroyOnLoad(canvas);
+        }
+        else
+        {
+           Destroy(this.gameObject);
+        }
+    }
+
+    public void AddHealth()
+    {
+        HP++;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene()
+    {
+        int level;
+        if (SceneManager.GetActiveScene().name != "Menu")
+        {
+            level = int.Parse(SceneManager.GetActiveScene().name);
+            level += 1;
+            if (level < 10)
+                SceneManager.LoadScene(level);
+        }
+        else
+            SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/_Scripts/HealthScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            GameManager.instance.HP--;
+        else if (Input.GetKeyDown(KeyCode.Return))
+            GameManager.instance.AddHealth();
+        if (GameManager.instance.HP == 0)
+            Destroy(gameObject);
+    }
+}
