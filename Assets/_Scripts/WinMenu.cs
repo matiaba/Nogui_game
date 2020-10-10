2da328452a7ca9a5bd1384fe87ef80bbad35682c
Matias Abarquez Mendoza <matias.abarquez@hotmail.com>
2020-10-09 22:19:53 -0300

    Fixed winmenu

--- Assets/_Scripts/WinMenu.cs
+++ Assets/_Scripts/WinMenu.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 public class WinMenu : MonoBehaviour
 {
     public GameObject panel_inventory;
-    public GameObject canvas;
-    public GameObject menu_ganar;
-    public GameObject menu_principal;
-    public GameObject scene_music;
-
-    AudioSource audio_scene;
+    public static GameManager gameManager;
+    public GameObject music;
     int inventory_slots;
-    // Start is called before the first frame update
+
     void Start()
     {
         panel_inventory = GameObject.FindGameObjectWithTag("Inventory");
         inventory_slots = panel_inventory.transform.childCount;
-        canvas = GameObject.FindGameObjectWithTag("Menu");
-        menu_ganar = canvas.transform.GetChild(3).gameObject;
-        menu_principal = canvas.transform.GetChild(4).gameObject;
-        scene_music = GameObject.Find("MusicScene");
-        audio_scene = scene_music.GetComponent<AudioSource>();
+        if (gameManager == null)
+        {
+            gameManager = FindObjectOfType<GameManager>();
+        }
     }
 
 
@@ -47,13 +42,10 @@ public class WinMenu : MonoBehaviour
 
         if (activos == inventory_slots)
         {
-            canvas.SetActive(true);
-            menu_ganar.SetActive(true);
-            menu_principal.SetActive(false);
             Time.timeScale = 0;
-            audio_scene.Stop();
-            Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            gameManager.menu_ganar.SetActive(true);
         }
 
         else
